4888d8073ae7df33a1a1768b2440bf4973227802
wangy <wangyouan@xmu.edu.cn>
2024-08-12 15:53:06 +0800

    initial update

---
+++ Constants/path_info.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+# @Filename: path_info
+# @Date: 2024/8/12
+# @Author: Mark Wang
+# @Email: wangyouan@gamil.com
+
+
+import os
+
+
+class PathInfo(object):
+    ROOT_PATH = r'D:\Onedrive\Temp\Projects\STOCKAct'
+    TEMP_PATH = os.path.join(ROOT_PATH, 'temp')
+    RESULT_PATH = os.path.join(ROOT_PATH, 'output')
