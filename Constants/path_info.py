d1abf1876a88735b2613fd31a5555274ea7492b1
user <wangyouan@xmu.edu.cn>
2024-11-01 10:58:15 +0800

    update path information

--- Constants/path_info.py
+++ Constants/path_info.py
@@ -11,11 +11,16 @@ import os
 
 
 class PathInfo(object):
-    ROOT_PATH = r'D:\Onedrive\Temp\Projects\STOCKAct'
+    if hasattr(os, 'uname') and os.uname().nodename == 'wyaamd-server001':
+        ROOT_PATH = '/home/user/projects/STOCKAct'
+        DATABASE_PATH = '/home/user/data'
+    else:
+        ROOT_PATH = r'D:\Onedrive\Temp\Projects\STOCKAct'
+        DATABASE_PATH = r'D:\Onedrive\Documents\data'
+
     TEMP_PATH = os.path.join(ROOT_PATH, 'temp')
     RESULT_PATH = os.path.join(ROOT_PATH, 'regression_data')
     DATA_PATH = os.path.join(ROOT_PATH, 'data')
     REGRESSION_RESULT_PATH = os.path.join(ROOT_PATH, 'regression_results')
 
-    DATABASE_PATH = r'D:\Onedrive\Documents\data'
     COMPUSTAT_PATH = os.path.join(DATABASE_PATH, 'compustat')
