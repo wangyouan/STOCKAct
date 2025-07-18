0363b477f753a97cad7ebe433d01cb22cc04a9f5
wangyouan <wangyouan0629@hotmail.com>
2025-07-18 15:58:56 +0800

    change folder name

---
+++ OrganizeData/ConstructRegressionFile/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+# @Filename: __init__.py
+# @Date: 2025/7/18
+# @Author: Mark Wang
+# @Email: wangyouan@gamil.com
---
+++ OrganizeData/ConstructRegressionFile/step01_construct_political_connection_regression_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+# @Filename: step01_construct_political_connection_regression_file
+# @Date: 2025/7/18
+# @Author: Mark Wang
+# @Email: wangyouan@gamil.com
